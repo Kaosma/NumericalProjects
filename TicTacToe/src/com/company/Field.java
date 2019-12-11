fd3baed43810b1ecb9334c26b480757a7d86b459
Erik Ugarte <erik-nu@hotmail.com>
2019-12-11 15:56:32 +0100

    More functions added

--- TicTacToe/src/com/company/Field.java
+++ TicTacToe/src/com/company/Field.java
@@ -1,15 +1,22 @@
 package com.company;
-
-import java.util.Arrays;
+import java.util.*;
 
 public class Field {
     private int size;
-    private Box [][] grid;
-    public int filled = 0;
+    private String [][] grid;
+    private int filled = 0;
+    private List<String> alphabet = Arrays.asList("A","B","C","D","E","F","G","H","I",
+                                                  "J","K","L","M","N","O","P","Q","R",
+                                                  "S","T","U","V","W","X","Y","Z");
+    private List<String> numlist = Arrays.asList("1","2","3","4","5","6","7","8","9","10",
+                                                "11","12","13","14","15","16", "17","18",
+                                                "19","20","21","22","23","24","25","26");
 
     //Constructor
     public Field(int size) {
-        this.size = size;
+        this.size = size+1;
+        this.grid = new String[this.size][this.size];
+        setCoordinateBorder(this.size);
     }
 
     //Getters and Setters
@@ -17,23 +24,58 @@ public class Field {
         return size;
     }
 
-    public void setSize(int size) {
-        this.size = size;
-    }
-
     public int getFilled() {
         return filled;
     }
 
-    public void setGrid() {
-        this.grid = new Box[size][size];
-    }
-
     public String getGrid() {
-        return Arrays.deepToString(grid).replace("],", "],\n");
+        return Arrays.deepToString(grid).replace("],", "\n").replace("[","").replace(",","").replace("]]","");
     }
 
     public void setFilled(int filled) {
         this.filled = filled;
+    }
+
+    //Setting the coordinate system around the board
+    public void setCoordinateBorder(int dimension){
+        this.grid[0][0] = " ";
+        for(int j = 1; j < dimension; j++) {
+            this.grid[0][j] = alphabet.get(j-1);
+        }
+        for(int i = 1; i < dimension; i++) {
+            this.grid[i][0] = numlist.get(i-1);
+        }
+        for(int i = 1; i < dimension; i++) {
+            for(int j = 1; j < dimension; j++) {
+                this.grid[i][j] = " ";
+            }
+        }
+    }
+
+    //Check Neighbor to see winning possibilty
+    public void checkNeighbor(int x, int y){
+        int currentX = x;
+        int currentY = y;
+        if(x+1 < size || y+1 < size) {
+            if(this.grid[x+1][y+1] == this.grid[x][y]) {
+            }
+        }
+    }
+    //Input method
+    public void input(User player, String coordinate){
+        String first = String.valueOf(coordinate.charAt(0)).toUpperCase();
+        String second = String.valueOf(coordinate.charAt(1));
+
+        int y = alphabet.indexOf(first)+1;
+        int x = numlist.indexOf(second)+1;
+
+        System.out.println(x);
+        System.out.println(y);
+        if(this.grid[x][y] == " "){
+            this.grid[x][y] = player.getCharacter();
+        } else{
+            System.out.println("FULL!");
+        }
+        checkNeighbor(x,y);
     }
 }
--- TicTacToe/src/com/company/Main.java
+++ TicTacToe/src/com/company/Main.java
@@ -1,23 +1,22 @@
 package com.company;
-
 import java.util.*;
 
 public class Main {
 
-    public static String updateGame(Field object){
+    public static String updateGame(Field object) {
         return object.getGrid();
     }
 
-    //main program
+    //Main program
     public static void main(String[] args) {
+        System.out.println();
         //Scanner for reading in the game size
         Scanner reader = new Scanner(System.in);
-        System.out.println("Please select size for your 2 dimensional game, the width and height will be the same: ");
+        System.out.println("Please select size for your 2 dimensional game, the width and height will be the same(MAX 26): ");
         int size = reader.nextInt();
 
         //Initiating field with game size
         Field game = new Field(size);
-        game.setGrid();
 
         //Read and initiate player 1
         System.out.println("Player 1 (☓) select name: ");
@@ -27,20 +26,26 @@ public class Main {
         System.out.println("Player 2 (◯) select name: ");
         User playerTwo = new User(reader.next(), "◯");
 
+        System.out.println(updateGame(game));
+
         //Running the game until player 1 or player 2 wins or until all boxes are filled
         while(!playerOne.isWon() && !playerTwo.isWon()) {
-            System.out.println("Player 1 choose coordinate (eg. a4): ");
-            //
+            System.out.println(playerOne.getName()+" choose coordinate (eg. a4): ");
+            String input = reader.next();
+            game.input(playerOne, input);
+            System.out.println(updateGame(game));
 
             if(!playerOne.isWon()){
-                System.out.println("Player 2 choose coordinate (eg. a4): ");
+                System.out.println(playerTwo.getName()+" choose coordinate (eg. a4): ");
+                input = reader.next();
+                game.input(playerTwo, input);
+                System.out.println(updateGame(game));
+                playerOne.setWon(true);
             }
             else {
-                playerOne.setWon(true);
                 break;
             }
-
         }
-        System.out.println(updateGame(game));
+
     }
 }
